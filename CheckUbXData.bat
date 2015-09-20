cc6cf5c3c109a6d4aad9a028157a7ea54fbc4cb1
DfAC <DfAC@users.noreply.github.com>
2015-09-20 12:09:20 +0100

    Added basic R code

--- CheckUbXData.bat
+++ CheckUbXData.bat
@@ -2,23 +2,28 @@ rem LKB 2014-2015 (C)
 rem Getting data from uBx 6.0 to R script
 @ECHO OFF
 
-
+rem first lets get RINEX out
 
 md SS
 rem uBlox
 FOR /f %%F IN ('dir /b *.ubx') DO (
 	ECHO**Processing uBlox %%F...
-	d:\Utils\TEQC\teqc.exe -ublox ubx +nav %%F.nav +obs %%F.obs %%F
+	d:\Utils\TEQC\teqc.exe -ublox ubx +nav %%~nF.nav +obs %%~nF.obs %%F
 rem -R %%F
 )
 	mv *.ubx SS\
-	ECHO.
+	ECHO.	
 
+
+rem Now lets extract values in data frame format
 
 FOR /f %%F IN ('dir /b *.obs') DO (
 
 	rem to start with process RINEX
-	d:\Utils\GPSToolkit\bin\RinDump.exe -obs COM11_150919_095723_first_test.ubx.15o C1C L1C S1C> SV.Rdmp
-	cat SV.Rdmp |tr -s " " "," | cut -d, -f2,4-19 | sed "1,7d" >SV.csv
+	d:\Utils\GPSToolkit\bin\RinDump.exe --ref "3850179.7690,79543.5650,5067398.3140" --obs %%F --nav %%~nF.nav C1C L1C S1C  > SV.Rdmp
+	rem ELE AZI RNG crash RinDump, RTKLib reads RAW ubx properly, check this
+	cat SV.Rdmp |tr -s " " "," | cut -d, -f2-19 | sed "1,8d" >SV.csv
 )
+
+del SV.Rdmp, *.obs, *.nav
 
